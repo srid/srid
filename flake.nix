4887de7462807736adb4dde87cc004ca4fbf5851
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2022-10-15 23:50:18 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -14,8 +14,8 @@
       imports = [ inputs.emanote.flakeModule ];
       perSystem = { self', pkgs, system, ... }: {
         emanote.sites."default" = {
-          path = ./content;
-          pathString = "./content";
+          layers = [ ./content ];
+          layersString = [ "./content" ];
           port = 9801;
           prettyUrls = true;
         };
