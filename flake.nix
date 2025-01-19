2c0d49cab36891cd4c18812323cd6e1fc929f8f3
Sridhar Ratnakumar <srid@srid.ca>
2025-01-19 16:50:11 -0500

    Use idiomprph branch

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   };
 
   inputs = {
-    emanote.url = "github:srid/emanote";
+    emanote.url = "github:srid/emanote/idiomorph";
     nixpkgs.follows = "emanote/nixpkgs";
     flake-parts.follows = "emanote/flake-parts";
   };
