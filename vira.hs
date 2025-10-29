092917744aa34c0f9f17eb765e5a325af6cd56c7
Sridhar Ratnakumar <srid@srid.ca>
2025-10-29 15:14:41 -0400

    ci: add vira

---
+++ vira.hs
@@ -0,0 +1,9 @@
+-- Pipeline configuration for Vira <https://vira.nixos.asia/>
+
+\ctx pipeline ->
+  pipeline
+    { build.systems = 
+        [ "x86_64-linux"
+        , "aarch64-darwin"
+        ]
+    }
